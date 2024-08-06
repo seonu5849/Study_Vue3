3d42ed0c9a623be93b889634c9b39a2a898eddca
seonoo <tjsdn22@naver.com>
2024-08-06 21:59:46 +0900

    learn: props and dynamic for and if

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,61 +1,45 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title red">Card title</h5>
+			<!-- type : news, notice -->
+			<span class="badge text-bg-secondary">{{
+				type === 'news' ? '뉴스' : '공지사항'
+			}}</span>
+			<h5 class="card-title mt-2">{{ title }}</h5>
 			<p class="card-text">
-				<!-- :class="classes.red"> -->
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
+				{{ contents }}
 			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
+			<a v-if="isLike === true" href="#" class="btn btn-danger">좋아요</a>
+			<a v-else href="#" class="btn btn-outline-danger">좋아요</a>
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
 console.log('AppCard module');
 export default {
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
+	},
 	setup() {
-		// const style = useCssModule('classes');
-		// console.log('style: ', style);
-		console.log('AppCard setup()');
-
-		const color = ref('red');
-		color.value = 'blue';
-		return { color };
+		return {};
 	},
 };
 </script>
 
-<!-- <style scoped>
-/* scoped 는 현재 vue에만 적용되도록 하는 것을 말한다. */
-/* scoped가 되는 경우에는 PostCSS가 적용되어서 그렇다. */
-.red {
-	color: red !important; /* !important는 다른 것들보다 우선순위를 높게 챙기라는 것을 의미 */
-}
-</style> -->
-
-<!-- <style module="classes">
-/* module속성은 CSS 모듈로 컴파일이 되어 CSS 클래스를 '$style' 객체의 속성으로 노출 */
-/* module속성에 값을 넣으면 '$style'이 아닌 다른 이름을 지정해서 사용할 수 있다. */
-.red {
-	color: red !important;
-}
-</style> -->
-
-<!-- <style module="classes">
-/* module속성은 CSS 모듈로 컴파일이 되어 CSS 클래스를 '$style' 객체의 속성으로 노출 */
-/* module속성에 값을 넣으면 '$style'이 아닌 다른 이름을 지정해서 사용할 수 있다. */
-.red {
-	color: red !important;
-}
-</style> -->
-
-<style>
-/* v-bind 사용시 동적으로 css값을 변경할 수 있다. */
-.red {
-	color: v-bind(color) !important;
-}
-</style>
+<style></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -3,22 +3,19 @@
 		<div class="container py-4">
 			<div class="row g-3">
 				<div class="col col-4">
-					<AppCard></AppCard>
+					<AppCard title="제목1" contents="내용1"></AppCard>
 				</div>
 				<div class="col col-4">
-					<AppCard></AppCard>
+					<!-- 반응형 객체를 넣을때는 :title (v-bind) -->
+					<AppCard :title="post.title" :contents="post.contents"></AppCard>
 				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
+				<div v-for="post in posts" :key="post.id" class="col col-4">
+					<AppCard
+						:title="post.title"
+						:contents="post.contents"
+						:type="post.type"
+						:isLike="post.isLike"
+					></AppCard>
 				</div>
 			</div>
 		</div>
@@ -26,6 +23,7 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
 import AppCard from './AppCard.vue';
 
 export default {
@@ -33,7 +31,29 @@ export default {
 		AppCard,
 	},
 	setup() {
-		return {};
+		const post = reactive({
+			title: '제목2',
+			contents: '내용2',
+		});
+
+		const posts = reactive([
+			{ id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'news' },
+			{
+				id: 4,
+				title: '제목4',
+				contents: '내용4',
+				isLike: true,
+				type: 'notice',
+			},
+			{
+				id: 5,
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+		return { post, posts };
 	},
 };
 </script>
