2bdc4e8dde92e7151596669e92d3a9afbfe8d412
seonoo <tjsdn22@naver.com>
2024-08-06 22:06:53 +0900

    learn: props -> setup(props) and computed

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,20 +2,21 @@
 	<div class="card">
 		<div class="card-body">
 			<!-- type : news, notice -->
-			<span class="badge text-bg-secondary">{{
-				type === 'news' ? '뉴스' : '공지사항'
-			}}</span>
+			<span class="badge text-bg-secondary">{{ typeName }}</span>
 			<h5 class="card-title mt-2">{{ title }}</h5>
 			<p class="card-text">
 				{{ contents }}
 			</p>
-			<a v-if="isLike === true" href="#" class="btn btn-danger">좋아요</a>
-			<a v-else href="#" class="btn btn-outline-danger">좋아요</a>
+			<!-- <a v-if="isLike === true" href="#" class="btn btn-danger">좋아요</a>
+			<a v-else href="#" class="btn btn-outline-danger">좋아요</a> -->
+			<a href="#" class="btn" :class="isLikeClass">좋아요</a>
 		</div>
 	</div>
 </template>
 
 <script>
+import { computed } from 'vue';
+
 console.log('AppCard module');
 export default {
 	props: {
@@ -38,9 +39,20 @@ export default {
 			type: Boolean,
 			default: false,
 		},
+		obj: {
+			type: Object,
+			default: () => {},
+		},
 	},
-	setup() {
-		return {};
+	setup(props) {
+		console.log(props.title);
+		const isLikeClass = computed(() =>
+			props.isLike ? 'btn-danger' : 'btn-outline-danger',
+		);
+		const typeName = computed(() =>
+			props.type === 'news' ? '뉴스' : '공지사항',
+		);
+		return { isLikeClass, typeName };
 	},
 };
 </script>
