24fa6ddc0690acc8dae05b6f73d14d95b48b3886
seonoo <tjsdn22@naver.com>
2024-08-17 13:44:40 +0900

    learn : inline emit

---
+++ src/components/event_components/TheView.vue
@@ -0,0 +1,75 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<PostCreate @create-post="createPost"></PostCreate>
+			<div class="row g-3">
+				<div class="col col-4">
+					<AppCard title="제목1" contents="내용1"></AppCard>
+				</div>
+				<div class="col col-4">
+					<!-- 반응형 객체를 넣을때는 :title (v-bind) -->
+					<AppCard :title="post.title" :contents="post.contents"></AppCard>
+				</div>
+				<div v-for="post in posts" :key="post.id" class="col col-4">
+					<AppCard
+						:title="post.title"
+						:contents="post.contents"
+						:type="post.type"
+						:is-like="post.isLike"
+						@toggle-like="post.isLike = !post.isLike"
+						:obj="obj"
+					></AppCard>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import AppCard from './AppCard.vue';
+import PostCreate from './PostCreate.vue';
+
+export default {
+	components: {
+		AppCard,
+		PostCreate,
+	},
+	setup() {
+		const post = reactive({
+			title: '제목2',
+			contents: '내용2',
+		});
+
+		const obj = reactive({
+			title: '제목2',
+			contents: '내용2',
+		});
+
+		const posts = reactive([
+			{ id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'news' },
+			{
+				id: 4,
+				title: '제목4',
+				contents: '내용4',
+				isLike: true,
+				type: 'notice',
+			},
+			{
+				id: 5,
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+
+		const createPost = (a, b, c, d) => {
+			alert(`${a}, ${b}, ${c}, ${d}`);
+		};
+		return { post, posts, obj, createPost };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
