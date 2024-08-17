6685eb6a8be63e8eab75cb7cff4a10a63e90e00c
seonoo <tjsdn22@naver.com>
2024-08-17 15:40:25 +0900

    chore : 디렉토리 변경

---
+++ src/lectures/이벤트/App_event.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<TheNav></TheNav>
+		<TheView></TheView>
+	</div>
+</template>
+
+<script>
+import TheNav from '@/components/event_components/TheNav.vue';
+import TheView from '@/components/event_components/TheView.vue';
+export default {
+	components: {
+		TheNav,
+		TheView,
+	},
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
