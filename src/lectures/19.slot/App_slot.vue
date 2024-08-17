2e1e60fc2d00a07ecf0e14a370989caf56fe397a
seonoo <tjsdn22@naver.com>
2024-08-17 18:11:27 +0900

    chore : 디렉토리 수정

---
+++ src/lectures/19.slot/App_slot.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<TheNav></TheNav>
+		<TheView></TheView>
+	</div>
+</template>
+
+<script>
+import TheNav from '@/components/slot_components/TheNav.vue';
+import TheView from '@/components/slot_components/TheView.vue';
+
+export default {
+	components: {
+		TheNav,
+		TheView,
+	},
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style scoped>
+.fancy-btn {
+	color: #fff;
+	background: linear-gradient(315deg, #42d392 25%, #647eff);
+	border: none;
+	padding: 5px 12px;
+	margin: 5px;
+	border-radius: 8px;
+	cursor: pointer;
+}
+</style>
