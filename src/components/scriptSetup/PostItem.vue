a70a0064ce433884a3c75dda124bbbd4eeeeed04
seonoo <tjsdn22@naver.com>
2024-09-02 22:14:49 +0900

    learn: export setup에서 script setup으로 변경

--- src/components/scriptSetup/PostItem.vue
+++ src/components/scriptSetup/PostItem.vue
@@ -17,55 +17,44 @@
 	</div>
 </template>
 
-<script>
+<script setup>
 import { computed } from 'vue';
 
-// console.log('AppCard module');
-export default {
-	props: {
-		type: {
-			type: String,
-			default: 'news',
-			validator: value => {
-				return ['news', 'notice'].includes(value);
-			},
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		contents: {
-			type: String,
-			// required: true,
-		},
-		isLike: {
-			type: Boolean,
-			default: false,
-		},
-		obj: {
-			type: Object,
-			default: () => {},
+const props = defineProps({
+	type: {
+		type: String,
+		default: 'news',
+		validator: value => {
+			return ['news', 'notice'].includes(value);
 		},
 	},
-	emit: ['toggleLike'],
-	setup(props, context) {
-		// props는 하위 컴포넌트에서는 조작할 수 없는 단방향 통신이다.
-		// emit을 통해 하위 컴포넌트에서 상위 컴포넌트로 이벤트를 보낼 수 있다.
-		// console.log(props.title);
-		const isLikeClass = computed(() =>
-			props.isLike ? 'btn-danger' : 'btn-outline-danger',
-		);
-		const typeName = computed(() =>
-			props.type === 'news' ? '뉴스' : '공지사항',
-		);
+	title: {
+		type: String,
+		required: true,
+	},
+	contents: {
+		type: String,
+		// required: true,
+	},
+	isLike: {
+		type: Boolean,
+		default: false,
+	},
+	obj: {
+		type: Object,
+		default: () => {},
+	},
+});
+const emit = defineEmits(['toggleLike']);
 
-		const toggleLike = () => {
-			// props.isLike = !props.isLike;
-			// props.obj.title = '김길동'; // 이렇게 사용하지 않도록 주의하자
-			context.emit('toggleLike');
-		};
-		return { isLikeClass, typeName, toggleLike };
-	},
+// console.log(props.title);
+const isLikeClass = computed(() =>
+	props.isLike ? 'btn-danger' : 'btn-outline-danger',
+);
+const typeName = computed(() => (props.type === 'news' ? '뉴스' : '공지사항'));
+
+const toggleLike = () => {
+	emit('toggleLike');
 };
 </script>
 
