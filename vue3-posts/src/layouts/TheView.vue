bc13d8f60bae45c262cd7015d404aebae4dcd9ff
seonoo <tjsdn22@naver.com>
2024-09-04 22:44:08 +0900

    driectory: 상위폴더로 묶음

---
+++ vue3-posts/src/layouts/TheView.vue
@@ -0,0 +1,12 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<!-- <h2>Hello Vue3</h2> -->
+			<router-view></router-view>
+		</div>
+	</main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
