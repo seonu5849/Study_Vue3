58cca2e39dcdcdfe6b669803f74c8436b8b5ac1e
seonoo <tjsdn22@naver.com>
2024-11-20 00:10:34 +0900

    learn: Not Found 와 중첩 라우트 적용 (탭기능)

--- vue3-posts/src/router/index.js
+++ vue3-posts/src/router/index.js
@@ -5,6 +5,11 @@ import PostCreateView from '@/views/posts/PostCreateView.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import PostEditView from '@/views/posts/PostEditView.vue';
 import PostListView from '@/views/posts/PostListView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import NestedView from '@/views/nested/NestedView.vue';
+import NestedOneView from '@/views/nested/NestedOneView.vue';
+import NestedTwoView from '@/views/nested/NestedTwoView.vue';
+import NestedHomeView from '@/views/nested/NestedHomeView.vue';
 
 const routes = [
 	{
@@ -37,6 +42,33 @@ const routes = [
 		name: 'PostEdit',
 		component: PostEditView,
 	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: NotFoundView
+	},
+	{
+		path: '/nested',
+		name: 'Nested',
+		component: NestedView,
+		children: [
+			{
+				path: '',
+				name: 'NestedHome',
+				component: NestedHomeView
+			},
+			{
+				path: 'one', // 여기에는 /one이 되면 안된다 (상대경로가 됨.), one만 적으면 /nested/one으로 자동으로 붙음
+				name: 'NestedOne',
+				component: NestedOneView
+			},
+			{
+				path: 'two',
+				name: 'NestedTwo',
+				component: NestedTwoView
+			}
+		],
+	}
 ];
 
 const router = createRouter({
---
+++ vue3-posts/src/views/nested/NestedView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <RouterLink class="nav-link" active-class="active" to="/nested/one">
+          Nated One
+        </RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" active-class="active" to="/nested/two">
+          Nated Two
+        </RouterLink>
+      </li>
+    </ul>
+    <hr class="my-4">
+    <!-- RouterLink를 클릭했을 때 생기는 화면 -->
+    <RouterView></RouterView> 
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
